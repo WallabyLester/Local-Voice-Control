0f81d972c3fba47378bc2fde8fcddfd84bc4fa61
Andru Liu <90433630+WallabyLester@users.noreply.github.com>
2024-10-15 22:20:17 -0700

    Commenting audio saving code.

--- save_audio.py
+++ save_audio.py
@@ -3,21 +3,31 @@ import tempfile
 import wave
 
 def recording_callback(indata, frames, time, status):
-    """ Place 5 second audio chunk in temp file."""
+    """ Callback for sounddevice input stream. 
+     
+    Parameters
+    ----------
+        indata : numpy.ndarray
+            Input audio.
+        frames : int
+            Frames in audio data.
+        time : CData
+            Current stream time.
+        status : CallbackFlags
+            Callback status.
+    """
     if status:
         print(status)
-    # Create tempfile, with autodeletion
-    with tempfile.NamedTemporaryFile(delete=True, suffix='.wav', prefix='audio_', dir='.') as tmpfile:
-        # save 5 second audio to .wav file
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.wav', prefix='audio_', dir='.') as tmpfile:
         with wave.open(tmpfile.name, 'wb') as wave_file:
-            wave_file.setnchannels(1) # mono audio
-            wave_file.setsampwidth(2) # 16-bit audio
+            wave_file.setnchannels(1)       # mono audio
+            wave_file.setsampwidth(2)       # 16-bit audio
             wave_file.setframerate(16000)   # sample rate
             wave_file.writeframes(indata)
 
 if __name__=="__main__":
     try:
-        # 5 second chunks of audio
         with sd.InputStream(callback=recording_callback, dtype='int16', channels=1, samplerate=16000, blocksize=16000*5):
             print("Recording...Press Ctrl+C to stop.")
             while True:
