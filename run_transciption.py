855e995316b13cac16bdfe4ded92f96e505ee7e7
Andru Liu <90433630+WallabyLester@users.noreply.github.com>
2024-10-15 18:08:52 -0700

    Made single transcription call example.

--- run_transciption.py
+++ run_transciption.py
@@ -4,11 +4,11 @@ from rt_audio_to_text import RTAudioToText
 
 if __name__=="__main__":
     rt_model = RTAudioToText()
-    try:
-        # 5 second chunks of audio
-        with sd.InputStream(callback=rt_model.recording_callback, dtype='int16', channels=1, samplerate=16000, blocksize=16000*5):
-            print("Recording...Press Ctrl+C to stop.")
-            while True:
-                pass
-    except KeyboardInterrupt:
-        print("Recording stopped.")
+    fs = 16000
+    duration = 2    # 2 second chunks of audio
+
+    with sd.InputStream(callback=rt_model.recording_callback, dtype='int16', channels=1, samplerate=fs, blocksize=fs*duration):
+        print("Recording...")
+        sd.sleep(duration * 1000) # in ms
+    
+    print(rt_model.output)
