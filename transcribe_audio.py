9627e9dd1caa0befc07889a1ec1bb76340af3a0b
Andru Liu <90433630+WallabyLester@users.noreply.github.com>
2024-10-15 22:21:53 -0700

    Removing unneeded file.

--- transcribe_audio.py
+++
@@ -1,9 +0,0 @@
-import whisper 
-
-# load the base model from Whisper
-model = whisper.load_model("tiny")
-
-audio = "test_audio.wav"
-
-result = model.transcribe(audio, fp16=False)
-print(result["text"])
